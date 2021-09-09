358c55881ca37470bfa5f2d4bb54150727f257c2
Fallaluc <Fallal@ucmail.uc.edu>
2021-09-09 13:33:23 -0400

    Update / endpoint

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -3,14 +3,30 @@ package com.plantplaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class PlantPlacesController {
 	
 	
+	/**
+	 * Handle the /start endpoint
+	 * @return
+	 *
+	 */
 	@RequestMapping("/start")
 	public String start() {
 		
 		return "start";
 	}
 
+	/**
+	 * Handle the / endpoint
+	 * @return
+	 *
+	 */
+	@RequestMapping("/")
+	public String index() {
+		
+		return "start";
+	}
 }
